4b1a73264c67ef587db441704ea83551030b70c8
Nii <100323103+Nimilanez@users.noreply.github.com>
2023-06-18 02:58:46 -0300

    Add files via upload

---
+++ DS/ExemploComboCheckRadio/src/main/java/com/mycompany/exemplojcomboCheckradio/ExemploComboCheckRadio.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.exemplojcomboCheckradio;
+
+/**
+ *
+ * @author User
+ */
+class ExemploComboCheckRadio {
+
+    void setDefaultCloseOperation(int EXIT_ON_CLOSE) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
