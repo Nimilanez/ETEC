616802400a6a4feb88c82c973b59901d09b6a123
Nii <100323103+Nimilanez@users.noreply.github.com>
2023-08-06 01:36:08 -0300

    Add files via upload

---
+++ DS/ControledeCaixa/src/Principal.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+public class Principal {
+   public static void main(String[] args) {
+        //Instancia o objeto MenuCalculadora
+        Menu menuPrincipal = new Menu();
+        menuPrincipal.executarCaixa();
+    }
+    
+}   
+
