5f6816d067ad3f7392f8e17dc830f8e0f0469797
Nii <100323103+Nimilanez@users.noreply.github.com>
2023-08-21 10:41:09 -0300

    Add files via upload

--- DS/IMC/src/MenuIMC.java
+++ DS/IMC/src/MenuIMC.java
@@ -85,7 +85,7 @@ public class MenuIMC {
         }
     }
 
-    void executarImc() {
+    void executarIMC() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
